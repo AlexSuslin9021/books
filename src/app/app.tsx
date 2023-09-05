5d02a29179f793990a622db5d74852525653dcc0
alexsuslin <alexsuslin@inbox.ru>
2023-09-05 09:25:07 +0300

    - create search.tsx search.module.css

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './app.module.css'
+import {Search} from "../search/search";
 
 function App() {
   return (
     <div className={s.app} >
-
+      <Search/>
     </div>
   );
 }
---
+++ src/search/search.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './search.module.css'
+export const Search = () => {
+    return (
+        <header className={s.search}>
+
+        </header>
+    );
+};
+
