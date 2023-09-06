86281bff974c547955306e76cd1271cd61477d0f
alexsuslin <alexsuslin@inbox.ru>
2023-09-06 09:32:10 +0300

    - create input.tsx

---
+++ src/search/Input/input.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from '../search.module.css'
+
+export const Input = () => {
+    return (
+        <input className={s.input} type={'text'}/>
+
+
+    );
+};
+
--- src/search/search.tsx
+++ src/search/search.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import s from './search.module.css'
+import {Input} from "./Input/input";
 export const Search = () => {
     return (
         <header className={s.search}>
-
+            <Input/>
         </header>
     );
 };
